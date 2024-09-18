4e4e7a612ed4155321502f7769001bc62331fe49
user.name:"DeepakGulati31 <user.email:"gulatideepak844@gmail.com>
2024-09-18 18:06:35 +0530

    final additions

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const io = socket(server);
 const chess = new Chess();
 
 let players = {};
-let currentPlayer = "W";
+let currentPlayer = "w";
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
@@ -21,7 +21,53 @@ app.get('/', (req, res) => {
 })
 
 io.on("connection", (uniquesocket) => {
-    io.emit("lololo")
+    console.log("connected")
+
+    if (!players.white) {
+        players.white = uniquesocket.id;
+        uniquesocket.emit("playerRole", "w");
+    } else if (!players.black) {
+        players.black = uniquesocket.id;
+        uniquesocket.emit("playerRole", "b");
+    } else {
+        uniquesocket.emit("spectatorRole");
+    }
+
+    uniquesocket.on("disconnect", () => {
+        if (uniquesocket.id === players.white) {
+            delete players.white;
+        }
+        else if (uniquesocket.id === players.black) {
+            delete players.black;
+        }
+    })
+
+    uniquesocket.on("move", (move) => {
+        try {
+            if (chess.turn() === 'w' && uniquesocket.id !== players.white) return;
+            if (chess.turn() === 'b' && uniquesocket.id !== players.black) return;
+
+            const result = chess.move(move);
+            if (result) {
+                currentPlayer = chess.turn();
+                io.emit("move", move);
+                io.emit("boardState", chess.fen());
+            } else {
+                console.log('invalid move:', move);
+                uniquesocket.emit("invalid move", move);
+
+            }
+        } catch (error) {
+            console.log(error);
+            uniquesocket.emit("Invalid move", move)
+
+        }
+    })
+
+    // io.emit("lololo")
+    // uniquesocket.on("disconnect", () => {
+    //     console.log("user disconnected")
+    // })
 })
 
 server.listen(3000, () => {
